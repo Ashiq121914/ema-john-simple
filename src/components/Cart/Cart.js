df6ceb06cce04853ee68a96827f33ccc1efa8a56
GaziAshiq <ashiq121914@gmail.com>
2022-09-29 08:53:25 +0600

    fechted data and showed them

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,25 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
-  console.log(cart.length);
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total += product.price * quantity;
+    shipping += product.shipping * quantity;
+  }
+  const tax = (total * 0.1).toFixed(2);
+  const grandTotal = total + shipping + parseFloat(tax);
   return (
-    <div>
+    <div className="cart">
       <h4>Order summary</h4>
-      <p>Selected Items:{cart.length}</p>
+      <p>Selected Items:{quantity}</p>
+      <p>Total price:${total} </p>
+      <p>Total Shipping:${shipping}</p>
+      <p>Tax:{tax}</p>
+      <h5>Grand Total:{grandTotal} </h5>
     </div>
   );
 };
